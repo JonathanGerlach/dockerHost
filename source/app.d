c949f0fddadc26d9869e38517910b701687cb635
Jonathan Gerlach <jonathan@thegerlach.net>
2017-04-27 14:08:16 -0400

    Set timeout correctly.

--- source/app.d
+++ source/app.d
@@ -51,7 +51,7 @@ int main(string[] args)
 			"DOCKER_HOST": "tcp://us-east-1.docker.joyent.com:2376",
 			"DOCKER_CLIENT_TIMEOUT": "300",
 			"DOCKER_TLS_VERIFY": "1",
-			"COMPOSE_HTTP_TIMEOUT": "1"
+			"COMPOSE_HTTP_TIMEOUT": "300"
 		],
 		"HomeWindows":
 		[
